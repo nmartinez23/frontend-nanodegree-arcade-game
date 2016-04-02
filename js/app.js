a2234b74e4bff09383067332be4642231657d8bb
Nick Martinez <nmartinez@Nicholass-MacBook-Air.local>
2016-04-02 11:36:18 -0700

    Added Enemy class variables
    Variables added for Enemy instances

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(posX, posY, GameSpeed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = posX;
+    this.y = posY;
+    this.speed = gameSpeed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -28,6 +31,7 @@ Enemy.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
 // Place the player object in a variable called player
 
 
